af180ceb119b904cf38b00a7583293d982e9e44c
EdgarRS <edgar_rrr@outlook.com>
2021-09-01 06:43:16 +0000

    [open] Abrir Archivos

---
+++ file-io/open-files.c
@@ -0,0 +1,34 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+
+
+int main(int argc, char const *argv[])
+{
+
+    printf("File IO\n");
+
+    // OPEN
+    int file1 = open("./text.txt",O_RDWR|O_CREAT,S_IRUSR|S_IWUSR);
+    /*
+          ^       ^        ^            ^               ^
+          |       |        |            |               |
+          FD     call     path      flag create      File Permissions
+    */
+
+    // Manage Errors
+    if (file1 == -1)
+    {
+        printf("Open file: %s",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    printf("File IO Open: OK\n");
+
+    close(file1);
+
+    return EXIT_SUCCESS;
+}
